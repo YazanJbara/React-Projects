8c6943c2e8de7e5b2ebcf26ac6b48780104a46c9
yazanjbara20034456@gmail.com <yazanjbara20034456@gmail.com>
2023-09-04 05:43:33 +0300

    Completed

--- eat-n-slip/src/App.js
+++ eat-n-slip/src/App.js
@@ -42,6 +42,15 @@ export default function App() {
 
   const [isAddFriendFormShown, SetisAddFriendFormShown] = useState(false);
 
+  /* Displaying new friend state */
+  /* the friends variable in the map now is undefined , so now we need to pass the state variable as a prop to the
+   FriendsList Component */
+  const [friends, setFriends] = useState(initialFriends);
+
+  // BCZ the form and the list are siblings we need lifted up state to make selecting friend work
+  // I don't need the FormSplitBill component to be not shown when no friend is selected , so I need to pass the state variable to that component
+  const [selectedFriend, setSelectedFriend] = useState(null); //null because by default no object (no one selected) , null is not working , "" works
+
   //declaring the updating function , the value is depending on the current state so it's the opposite of the default (setter function)
   // or You can use callback function  , true will work but I need callback to set the current value and the opposite of it
   function handleShowAddFriendForm() {
@@ -49,40 +58,87 @@ export default function App() {
     SetisAddFriendFormShown((show) => !show);
   }
   // finally I want a close text instead of Add friend when the form is shown (conditional rendering)
+  // adding a friend handle function to update the UI , I will pass the friend object that we created in the map (argument)
+  // here the current is actually the current friends list , the friend is the new friend
+  function handleAddFriend(friend) {
+    setFriends((friends) => [...friends, friend]);
+    SetisAddFriendFormShown(false); // to make the another form close when i hit select (if it's opened)
+  }
+
+  //function to update the setter function of the selection , the friend object is assigned to when select happen
+  // now pass it down to FriendList and Friends  Components
+  function handleSelection(friend) {
+    //this code is for updating the setter function and for when i click close it will close the form , the ? after selected is optional chaining to solve null issue of can't read the properties
+    setSelectedFriend((selected) =>
+      selected?.id === friend.id ? null : friend
+    );
+    SetisAddFriendFormShown(false);
+  }
+  
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+  }
   return (
     <div className="app">
       <div className="sidebar">
-        <FriendsList />
-        {/* the state variable is used , next I need To update the variable */}
-        {isAddFriendFormShown && <FormAddFriend />}
+        <FriendsList
+          friends={friends}
+          onSelectFriend={handleSelection} //pass this to FriendList
+          selectedFriend={selectedFriend}
+        />
+        {/* the state variable is used , next I need To update the variable , Here also I will pass the update function , the onAddFriend is like onClick but customized for me , and this one what I will pass down */}
+        {isAddFriendFormShown && (
+          <FormAddFriend onAddFriend={handleAddFriend} />
+        )}
         {/* define it  , pass the function*/}
         <Button onClick={handleShowAddFriendForm}>
           {isAddFriendFormShown ? "Close" : "Add Friend"}
         </Button>
       </div>
-      <FormSplitBill />
+      {/* the selected friend should now receive the current friend and display it's name in the FormSplitBill component */}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
-
-function FriendsList() {
-  const friends = initialFriends;
+// destructure the state variable to make the map work
+// now I need to give the form ability to add friends , u have to ways , you can pass the setter function to the form
+// or you can make an event handler function in the parent
+function FriendsList({ friends, onSelectFriend, selectedFriend }) {
+  // (lifted) const friends = initialFriends;
   return (
     // I created a variable to assign the array into it to use it later
     // I made a loop on the array using friends variable then I give it an Immutable array to destructure it
     // I rendered the component Friends to pass the immutable array as a prop into it and destructure it in the Friends Component
     <ul>
       {friends.map((friend) => (
-        <Friends friend={friend} key={friend.id} />
+        <Friends
+          friend={friend}
+          key={friend.id}
+          onSelectFriend={onSelectFriend}
+          selectedFriend={selectedFriend}
+        />
       ))}
     </ul>
   );
 }
 
 // Passing the prop to begin destructuring the list rendering as an li
-function Friends({ friend }) {
+// here where we need to compare the selected for each one in the list
+function Friends({ friend, onSelectFriend, selectedFriend }) {
+  const isSelected = selectedFriend?.id === friend.id; // this variable will be used to conditionally render a css class
   return (
-    <li>
+    <li className={isSelected ? "selected" : ""}>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {/* now conditional rendering to show who owe me and which one I owe  */}
@@ -99,7 +155,10 @@ function Friends({ friend }) {
       {friend.balance === 0 && <p>You And {friend.name} Are Even</p>}
       {/* because I will use the same button over and over I will but it in a
       component , the children in this case is Select and ofc I can change it */}
-      <Button>Select</Button>
+      {/* the button when clicked it will take the current friend then will save it to the state */}
+      <Button onClick={() => onSelectFriend(friend)}>
+        {isSelected ? "Close" : "Select"}
+      </Button>
     </li>
   );
 }
@@ -115,19 +174,25 @@ in other siblings , so I will define it in the App component then pass the state
 // Firstly I need to use controlled elements which I can use the values of the form in my application (on piece of state for each input)
 // thus the values will be synced with state
 
-function FormAddFriend() {
-  
+function FormAddFriend({ onAddFriend }) {
   const [name, setName] = useState("");
   const [image, setImg] = useState("https://i.pravatar.cc/48?u=499476");
-
 
   function PreventDefaultReloadingandResetAndAddingFriend(event) {
     event.preventDefault();
     // If I click the button with nothing added it will still execute the code under , so i need if to prevent this
     if (!name || !image) return;
-    // I need to create brand new list (object) for that new friend
+
     const id = crypto.randomUUID();
 
+    // I need to create brand new list (object) for that new friend
+    /* now I need the object (newFriend object) to be displayed in the page when I click to add friend
+    thus , I need state , the FriendList Component who needs to display the new friend , but I can't pass
+    props because they are siblings components , now we need this functionality as a lifted up state.
+    firstly , you need to git rid of  const friends = initialFriends; , this variable will be a lifted up state.
+    the Update happens here in this Component but the display in FriendList Component
+
+    */
     const newFriend = {
       id,
       name,
@@ -135,6 +200,8 @@ function FormAddFriend() {
       balance: 0,
     };
     // works , I need now to make the input fields back to empty when I click the button
+    // Adding a new friend
+    onAddFriend(newFriend);
     setName("");
     setImg("https://i.pravatar.cc/48?u=499476");
   }
@@ -163,24 +230,55 @@ function FormAddFriend() {
     </form>
   );
 }
+// receiving the selected friend , the .name part because we pass the friend object in the selection function
+// the part of mark the selected friend and changing text to close.
+// how do the application well know that it is currently selected?
+// first pass the selected friend to the friend object then compare that with each friend (each component that received the onselect needs the selected)
+// here I need to create controlled elements to let react handle the input values
 
-function FormSplitBill() {
+function FormSplitBill({ selectedFriend, onSplitBill }) {
+  function handleSubmitSplitBill(e) {
+    e.preventDefault();
+    if (!bill || !expenseByUser) return;
+    onSplitBill(whoisPaying === "user" ? paidByFriend : -expenseByUser);
+  }
+  const [bill, setBill] = useState("");
+  const [expenseByUser, setExpenseByUser] = useState("");
+  const paidByFriend = bill ? bill - expenseByUser : ""; //derived state
+  const [whoisPaying, setWhoisPaying] = useState("user");
   return (
-    <form className="form-split-bill">
-      <h2>Split A Bill With X </h2>
+    <form className="form-split-bill" onSubmit={handleSubmitSplitBill}>
+      <h2>Split A Bill With {selectedFriend.name} </h2>
       <label>💵 Bill Value</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
 
       <label>❌ Your Expense</label>
-      <input type="text" />
+      <input
+        type="text"
+        value={expenseByUser}
+        onChange={(e) =>
+          setExpenseByUser(
+            Number(e.target.value) > bill
+              ? expenseByUser
+              : Number(e.target.value)
+          )
+        }
+      />
 
-      <label>🧑‍🤝‍🧑 X Expense</label>
-      <input type="text" disabled />
+      <label>🧑‍🤝‍🧑 {selectedFriend.name} Expense</label>
+      <input type="text" disabled value={paidByFriend} />
 
       <label>🤑 Who is Paying the Bill?</label>
-      <select>
+      <select
+        value={whoisPaying}
+        onChange={(e) => setWhoisPaying(e.target.value)}
+      >
         <option value="user">You</option>
-        <option value="friend">X</option>
+        <option value="friend">{selectedFriend.name}</option>
       </select>
       <Button>Split Bill</Button>
     </form>
