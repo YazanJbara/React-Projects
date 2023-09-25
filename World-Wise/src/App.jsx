cc1cffaa8e6d46f293fdedbde9f3248d3d2f67a1
yazanjbara20034456@gmail.com <yazanjbara20034456@gmail.com>
2023-09-25 17:47:35 +0300

    initializing new components

--- World-Wise/src/App.jsx
+++ World-Wise/src/App.jsx
@@ -1,20 +1,56 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from './Pages/Product'
-import Pricing from './Pages/Pricing'
-import Login from './Pages/Login'
-import Homepage from './Pages/Homepage'
-import AppLayout from './Pages/AppLayout'
-import PageNotFound from './Pages/PageNotFound'
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import Login from "./Pages/Login";
+import Homepage from "./Pages/Homepage";
+import AppLayout from "./Pages/AppLayout";
+import PageNotFound from "./Pages/PageNotFound";
+import CityList from "./Components/CityList";
+import { useEffect, useState } from "react";
+
+const BaseURL = "http://localhost:8000";
 function App() {
+  // fetching from the fake API
+  const [cities, setCities] = useState([]);
+  const [isLodaing, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BaseURL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading data ):");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
-        <Route path="product" element={<Product/>}/>
-        <Route path="pricing" element={<Pricing/>}/>
-        <Route path="/login" element = {<Login/>} />
-        <Route path="app" element={<AppLayout/>}/>
-        <Route path="*" element={<PageNotFound/>}/>
+        <Route index element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+        {/* nested Routes here */}
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLodaing={isLodaing} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLodaing={isLodaing} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- World-Wise/src/Components/SideBar.jsx
+++ World-Wise/src/Components/SideBar.jsx
@@ -2,12 +2,17 @@ import styles from './SideBar.module.css'
 import Logo from './Logo'
 import AppNav from './AppNav'
 import Footer from './Footer'
+import { Outlet } from 'react-router-dom'
 function SideBar() {
     return (
         <div className={styles.sidebar}>
             <Logo/>
             <AppNav/>
-            <p>List of Cities</p>
+            {/* instead of hard coded p element we will use the Outlet to view what is inside the nested route 
+                so any of the nested routes if you navigate to it it will show the content inside this route in the sidebar            
+            */}
+            
+            <Outlet/>
             <Footer/>
 
         </div>
